235bf243e9b1ebd06fca6fb6134c9f619b809f3a
jryhdzh <zhanghang@jr1.cn>
2017-08-07 22:04:22 +0800

    crontab

--- timer_send.py
+++ timer_send.py
@@ -3,6 +3,6 @@ from app import text_reply
 itchat.auto_login(hotReload=True)
 friendList = itchat.get_friends(update=True)
 for friend in friendList:
-    if (friend['NickName'] == 'Sawyer'):
+    if friend['NickName'] in ['Sawyer']:
         text_reply('早上好')
 
